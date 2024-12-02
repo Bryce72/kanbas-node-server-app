5746c49017a977a096b77c494d264147ff5f2282
Bryce72 <86679354+Bryce72@users.noreply.github.com>
2024-12-02 13:48:22 -0500

    consolog signin

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -71,6 +71,7 @@ export default function UserRoutes(app) {
     const signin = async (req, res) => {
         const { username, password } = req.body;
         const currentUser = await dao.findUserByCredentials(username, password);
+        console.log("current user: ", currentUser)
         if (currentUser) {
             req.session["currentUser"] = currentUser;
             res.json(currentUser);
