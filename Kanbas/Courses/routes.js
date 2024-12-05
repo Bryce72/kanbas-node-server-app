3da11efc657afeec00aaea29d301f867c23a2b1b
Bryce72 <86679354+Bryce72@users.noreply.github.com>
2024-12-04 20:01:45 -0500

    onto 5.3.5

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,5 +1,6 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
+​​import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
 
@@ -8,6 +9,11 @@ export default function CourseRoutes(app) {
 
 app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    }
+ 
     res.json(course);
 });
  
